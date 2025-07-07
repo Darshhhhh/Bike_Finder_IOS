951fcb71c8de95e8e687885ec46bf451bf0381dc
Darshhhhh <contact.shahdarsh@gmail.com>
2025-07-07 12:19:20 -0400

    Initial Commit

---
+++ Exam_Darsh_Shah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exam_Darsh_Shah
+//
+//  Created by Darsh on 2025-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Exam_Darsh_Shah/Exam_Darsh_ShahApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exam_Darsh_ShahApp.swift
+//  Exam_Darsh_Shah
+//
+//  Created by Darsh on 2025-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Exam_Darsh_ShahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
